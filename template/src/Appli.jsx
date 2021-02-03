52ef95f57074681a2e5c8a3f616b00bb4e818893
Camille Semaan <cdem@xvitae.com>
2021-02-03 17:39:39 -0500

    Amélioration et simplification.

---
+++ template/src/Appli.jsx
@@ -0,0 +1,15 @@
+import logo from './logo-tim.png';
+import './Appli.scss';
+
+function Appli() {
+  return (
+    <div className="Appli">
+        <img src={logo} alt="TIM" />
+        <p>
+          Gabarit de base d'une Application React
+        </p>
+    </div>
+  );
+}
+
+export default Appli;
